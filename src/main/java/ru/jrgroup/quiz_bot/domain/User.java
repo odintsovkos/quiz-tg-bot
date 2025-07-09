91d4274fe9aea8fd63d0bbd349a2d4b91d10355b
R4Y <r4yx@ya.ru>
2025-07-09 22:12:40 +0300

    Добавлены аннотации NotBlank, NotNull, PastOrPresent, изменен конструктор с учотом аннотаций

--- src/main/java/ru/jrgroup/quiz_bot/domain/User.java
+++ src/main/java/ru/jrgroup/quiz_bot/domain/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,16 +14,11 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 /**
+ * JPA-сущность "Пользователь".
  * JPA-сущность "Пользователь", хранит основную информацию о пользователе.
  *
  * - id (Long)
  * - telegramId (Long)
- * - username (String)
- * - firstName (String)
- * - lastName (String)
- * - createdAt (Timestamp)
- * - lastActiveAt (Timestamp)
- *
  * Аннотации: @Entity, @Id
  */
 
@@ -35,11 +33,13 @@ public class User {
      */
     @Id
     @GeneratedValue
+    @NotNull
     private Long id;
 
     /**
      * Id пользователя в Telegram.
      */
+    @NotNull
     private Long telegramId;
 
     /**
@@ -51,6 +51,7 @@ public class User {
     /**
      * Имя пользователя, указанное в Telegram.
      */
+    @NotBlank
     private String firstName;
 
     /**
@@ -62,41 +63,37 @@ public class User {
     /**
      * Дата и время создания записи о пользователе.
      */
+    @PastOrPresent
     private LocalDateTime createdAt;
 
     /**
      * Дата и время последней активности пользователя.
      */
+    @PastOrPresent
     private LocalDateTime lastActiveAt;
 
 
     /**
      * Пустой конструктор для создания пользователя
-    */
+     */
     public User() {
     }
 
     /**
      * Конструктор для создания пользователя с заданными данными.
      *
-     * @param telegramId    Telegram ID пользователя
-     * @param username      Имя пользователя в Telegram (username)
-     * @param firstName     Имя пользователя
-     * @param lastName      Фамилия пользователя
-     * @param createdAt     Дата и время создания записи
-     * @param lastActiveAt  Дата и время последней активности пользователя
+     * @param telegramId   Telegram ID пользователя
+     * @param username     Имя пользователя в Telegram (username)
+     * @param firstName    Имя пользователя
+     * @param lastName     Фамилия пользователя
+     * @param createdAt    Дата и время создания записи
+     * @param lastActiveAt Дата и время последней активности пользователя
      * Необязательные поля могут содержать null.
+     * Валидация обязательных полей выполняется через аннотации
      */
 
     public User(Long telegramId, String username, String firstName, String lastName,
                 LocalDateTime createdAt, LocalDateTime lastActiveAt) {
-        if (telegramId == null) {
-            throw new IllegalArgumentException("Telegram ID cannot be null.");
-        }
-        if (firstName == null || firstName.trim().isEmpty()) {
-            throw new IllegalArgumentException("First name cannot be null.");
-        }
-
         this.telegramId = telegramId;
         this.username = username;
         this.firstName = firstName;
