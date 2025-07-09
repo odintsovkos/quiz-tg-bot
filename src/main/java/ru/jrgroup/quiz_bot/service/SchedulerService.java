5dd317d70a0eca33e4b8c9073281829569f5daad
Konstantin Odintsov <kos5172@yandex.ru>
2025-07-09 23:52:40 +0300

    Add logging configuration and enable scheduling for quiz bot

--- src/main/java/ru/jrgroup/quiz_bot/service/SchedulerService.java
+++ src/main/java/ru/jrgroup/quiz_bot/service/SchedulerService.java
@@ -1,11 +1,25 @@
 package ru.jrgroup.quiz_bot.service;
 
+import org.jvnet.hk2.annotations.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Сервис для задач по расписанию.
  *
  * - scheduleDailyQuiz() — планирование ежедневной рассылки викторин
  * - sendQuizToAllUsers() — рассылка викторин всем пользователям
  */
-
+@Service
 public class SchedulerService {
+    private static final Logger log = LoggerFactory.getLogger(SchedulerService.class);
+
+    public void sendDailyQuiz() {
+        log.info("The daily quiz has started sending out");
+        try {
+
+        } catch (Exception e) {
+            log.error("Ошибка при отправке вопроса", e);
+        }
+    }
 }
